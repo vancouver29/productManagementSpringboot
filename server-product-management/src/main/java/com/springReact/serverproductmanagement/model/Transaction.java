9925001200e596d122fb0da6889c41e1c1c23902
vancouver29 <vancouver2990@gmail.com>
2020-11-25 19:51:07 +0100

    created Transaction model

---
+++ server-product-management/src/main/java/com/springReact/serverproductmanagement/model/Transaction.java
@@ -0,0 +1,27 @@
+package com.springReact.serverproductmanagement.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data // for setter, getter, equals
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "purchase_date")
+    private LocalDateTime purchaseDate;
+}
